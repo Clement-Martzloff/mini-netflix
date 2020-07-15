cd3cc01914f00915c56e040d9e57b66d45c95421
Clement-Martzloff <cmrtzlff@gmail.com>
2020-07-15 16:49:27 +0200

    refactor: use exports default as component name

---
+++ src/components/index.tsx
@@ -0,0 +1,4 @@
+export { default as Header } from './header/Header'
+export { default as MovieDetail } from './movie-detail/MovieDetail'
+export { default as Movie } from './movie-list/Movie'
+export { default as MovieList } from './movie-list/MovieList'

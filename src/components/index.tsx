0810f3f050b91e2f8b4dcf339f19a5147b9022b5
Clement-Martzloff <cmrtzlff@gmail.com>
2020-07-17 16:16:14 +0200

    refactor: export more components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,6 @@
 export { default as Header } from './header/Header'
+export { default as NavMenu } from './nav-menu/NavMenu'
+export { default as Content } from './content/Content'
 export { default as MovieDetail } from './movie-detail/MovieDetail'
 export { default as Movie } from './movie-list/Movie'
 export { default as MovieList } from './movie-list/MovieList'

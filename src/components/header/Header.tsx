a92a952654a018502f6bf0df4b4f0e2fcf8de76c
Clement-Martzloff <cmrtzlff@gmail.com>
2020-07-17 16:16:50 +0200

    feat: create Header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,34 +1,61 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
+import clsx from 'clsx'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}))
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import { makeStyles } from '@material-ui/core/styles'
+import useDrawer from '../../hooks/UseDrawer'
 
 function Header() {
+  const { drawerState, dispatch, TOGGLE_DRAWER } = useDrawer()
+  const useStyles = makeStyles((theme) => ({
+    appBar: {
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+    appBarShift: {
+      width: `calc(100% - ${drawerState.drawerWidth}px)`,
+      marginLeft: drawerState.drawerWidth,
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    hide: {
+      display: 'none',
+    },
+  }))
   const classes = useStyles()
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            Header
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <AppBar
+      position="fixed"
+      className={clsx(classes.appBar, {
+        [classes.appBarShift]: drawerState.open,
+      })}
+    >
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          onClick={() => dispatch({ name: TOGGLE_DRAWER })}
+          edge="start"
+          className={clsx(classes.menuButton, drawerState.open && classes.hide)}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" noWrap>
+          mini-netflix
+        </Typography>
+      </Toolbar>
+    </AppBar>
   )
 }
 

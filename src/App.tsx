0a80a3eea78c686d1e4b415c8b0dfe51d131aeea
Clement-Martzloff <cmrtzlff@gmail.com>
2020-07-17 16:15:14 +0200

    feat: use ThemeProvider in the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,36 @@
 import React from 'react'
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
-import { Header, MovieDetail, MovieList } from './components'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import {
+  createMuiTheme,
+  ThemeProvider,
+  makeStyles,
+} from '@material-ui/core/styles'
+import { Header, NavMenu, Content } from './components'
+import { DrawerContextProvider } from './contexts/DrawerContext'
 
 /**
  * TODO: expand material-ui theme
  */
 const theme = createMuiTheme({})
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+  },
+}))
 
 function App() {
+  const classes = useStyles()
+
   return (
     <ThemeProvider theme={theme}>
-      <Header />
-      <MovieList />
-      <MovieDetail />
+      <div className={classes.root}>
+        <CssBaseline />
+        <DrawerContextProvider>
+          <Header />
+          <NavMenu />
+          <Content />
+        </DrawerContextProvider>
+      </div>
     </ThemeProvider>
   )
 }
